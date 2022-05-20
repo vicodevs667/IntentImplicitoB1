928dd53f7389b3cd4845fd2c760c53b7dbb03e45
Victor <victorsanjines667@gmail.com>
2022-05-20 10:34:16 -0400

    permisos de camara y sacar fotografia

--- app/src/main/java/com/example/intentimplicitob1/MainActivity.java
+++ app/src/main/java/com/example/intentimplicitob1/MainActivity.java
@@ -1,22 +1,33 @@
 package com.example.intentimplicitob1;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //Atributos que representas sus vistas
     private EditText etTelefono;
     private ImageButton btnLlamar, btnCamara;
+    private ImageView ivImagen;
 
     private String numeroTelefono;
+    //Codigo Constante para servicio de llamada
+    private final int PHONE_CODE = 100;
+    //Codigo Constante para activar camara
+    private final int CAMERA_CODE = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,32 @@ public class MainActivity extends AppCompatActivity {
             obtenerInformacion();
             activarServicioLlamada();
         });
+
+        btnCamara.setOnClickListener(view -> {
+            activarServicioCamara();
+        });
+    }
+
+    private void activarServicioCamara() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case CAMERA_CODE:
+                if(resultCode == Activity.RESULT_OK) {
+                    Bitmap foto = (Bitmap) data.getExtras().get("data");
+                    ivImagen.setImageBitmap(foto);
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
+
     }
 
     private void activarServicioLlamada() {
@@ -36,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
             //su forma de trabajar
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 //para versiones nuevas
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE},PHONE_CODE);
             } else {
                 //para versiones antiguas
                 configurarVersionAntigua();
@@ -63,12 +101,48 @@ public class MainActivity extends AppCompatActivity {
         etTelefono = findViewById(R.id.etTelefono);
         btnCamara = findViewById(R.id.btnCamara);
         btnLlamar = findViewById(R.id.btnLlamar);
+        ivImagen = findViewById(R.id.ivImagen);
     }
 
+    //Evaluan permisos para versiones antiguas
     private boolean revisarPermisos(String permiso) {
         //Valor entero que representa el permiso requerido en nuestra aplicacion
         int valorPermiso = this.checkCallingOrSelfPermission(permiso);
         return valorPermiso == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //Verificar el servicio a evaluar
+        switch (requestCode) {
+            case PHONE_CODE:
+                String permiso = permissions[0];
+                int permisoOtorgado = grantResults[0];
+                //asegurarse que para llamadas van a evaluar el permiso del servicio de Llamada
+                if (permiso.equals(Manifest.permission.CALL_PHONE)) {
+                    //Evaluar si el permiso ha sido otorgado o denegado
+                    if (permisoOtorgado == PackageManager.PERMISSION_GRANTED) {
+                        Intent intentLlamada = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+ numeroTelefono));
+                        startActivity(intentLlamada);
+                    } else {
+                        Toast.makeText(this, "El permiso esta denegado", Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+            case CAMERA_CODE:
+                int valor = grantResults[0];
+                if (valor == PackageManager.PERMISSION_GRANTED) {
+                    Intent intentCamara = new Intent( "android.media.action.IMAGE_CAPTURE");
+                    startActivityForResult(intentCamara, CAMERA_CODE);
+                } else {
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_LONG).show();
+                }
+
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
     }
 }
 
